ae3ec267e52ca5028457cb021921cc02867d04c1
Alex Eagleson <alexeagleson@gmail.com>
2021-12-22 01:41:45 -0500

    update script.js

--- public/script.js
+++ public/script.js
@@ -7,9 +7,6 @@ fetch("/employees")
       const card = cardTemplate.content.cloneNode(true);
       card.querySelector('h4').innerText = employee.name;
       card.querySelector('p').innerText = employee.title;
-
-      // const p = document.createElement("p");
-      // p.innerText = employee.name;
       document.body.appendChild(card);
     });
   });
